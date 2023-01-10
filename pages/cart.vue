8db9d185bd1521c92574675bbf9791e6689729a2
Felix <vadim200608@yandex.ru>
2023-01-10 17:48:23 +0400

    first commit

---
+++ pages/cart.vue
@@ -0,0 +1,100 @@
+<template>
+    <div id="cart">
+
+        <header>
+           <ul class="nav-list">
+               <li class="nav-item">
+                   <div class="burger-menu"></div>
+               </li>
+               <li class="nav-item">
+                   <h1>MADE</h1>
+               </li>
+               <li class="nav-item">
+                   <p>BASKET 2</p>
+               </li>
+           </ul>
+       </header>
+       <main>
+           <section class="slider">
+           <img class="img" src="../assets/imgs/cream-img.png" alt="">
+               <article>
+               <div class="slide-count">
+                <div class="index">
+                    <span class="current">{{ this.current }}</span>
+                    <div class="next"></div>
+                </div>
+                   <span class="count-line"></span>
+                   <span class="total">{{ this.total }}</span>
+               </div>
+               <div class="slide-title">
+                   <span>
+                   <span class="current">J014</span> 
+                   </span>
+                   Chair
+               </div>
+               <div class="slide-descriptions">
+                    <p class="curret">With the comfort of a lounge chair and the functionality of a dining chair, HAY’s reproduction of Poul M. Voulter’s J110 offers equal measures of strong aesthetics and practicality. Long rods at the back and curved armrests create an inviting expression. Made in lacquered solid beech and available in a variety of colours.</p>
+                    <p class="next">next</p>
+               </div>
+               <a href="#" class="btn">FIND A DEALER</a>
+               <div class="dots">
+                   <span class="dot dot-active"></span>
+                   <span class="dot"></span>
+                   <span class="dot"></span>
+               </div>
+               <div class="side-nav">
+                   <button class="btn-prev"  @click="prev()">
+                   <!-- <img src="../assets/imgs/up-arrow.png" alt=""> -->
+                   </button>
+                   <button class="btn-next">
+                   <!-- <img src="../assets/imgs/up-arrow.png" alt=""> -->
+                   </button>
+   
+               </div>
+               <div class="category">SEATING</div>
+               </article>
+           </section>
+       </main>
+    </div>
+</template>
+
+<script>
+
+import "../assets/styles/cart.scss"
+export default{
+    name:'cart',
+    data(){
+        return{
+            current:0,
+            total:3,
+            
+            currentDesc:'With the comfort of a lounge chair and the functionality of a dining chair, HAY’s reproduction of Poul M. Voulter’s J110 offers equal measures of strong aesthetics and practicality. Long rods at the back and curved armrests create an inviting expression. Made in lacquered solid beech and available in a variety of colours.'
+        }
+    },
+    methods:{
+        prev(){
+            let tl = this.$gsap.timeline();
+            tl.fromTo(
+                ".next",
+                { y:0 },
+                { y: -20 }
+            )
+            .fromTo(
+                ".next",
+                { opacity: 0 },
+                { opacity: 1 }
+            )
+            .fromTo(
+                ".next",
+                { y:-20 },
+                { y:0, duration:2 }
+            )
+        }
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+@import '../assets/styles/cart.scss';
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="main">
+    <aside>
+      <a href="#">LOGO</a>
+      <a href="#">About</a>
+      <a href="#"></a>
+    </aside>
+    <main>
+      <nav>
+        <ul>
+          <li class="liOne"></li>
+          <li class="liTwo"></li>
+          <li class="liThree"></li>
+        </ul>
+      </nav>
+
+      <NuxtLink to="/cart" class="title" style="cursor: pointer" @click="change()">
+        <h1>X</h1>
+        <div class="title-wrapper">
+          <span>U</span>
+          <span>M</span>
+          <span>G</span>
+        </div>
+      </NuxtLink>
+      <NuxtLink to="/cart" class="view">
+          <div class="circle">
+            <div class="arrow"></div>
+          </div>
+          <a>View Project</a>
+       
+      </NuxtLink>
+    </main>
+    <div class="contacts">
+      <a class="contacts-text">Contacts</a>
+     
+    </div>
+  </div>
+</template>
+
+<script>
+import "../assets/styles/sidebar.scss";
+import "../assets/styles/contacts.scss";
+
+export default {
+  name: "Index",
+  // mounted() {
+  //   this.boxRotation();
+  // },
+
+  methods: {
+    boxRotation() {
+      let tl = this.$gsap.timeline();
+      tl.fromTo(
+        "span",
+        { opacity: 0, y: -350, x: -100 },
+        { opacity: 1, y: 0, x: 0, duration: 1, rotation: "360" }
+      )
+        .fromTo(
+          "h1",
+          { opacity: 0, y: 350, x: 100 },
+          { opacity: 1, y: 0, x: 0, duration: 1 }
+        )
+        .fromTo(
+          "li",
+          { opacity: 0, scale: 0 },
+          { opacity: 1, scale: 1, duration: 1 }
+        )
+        .fromTo("a", { opacity: 0, y: -100 }, { opacity: 1, y: 0, duration: 1 })
+        .fromTo(
+          ".liOne",
+          { opacity: 0 },
+          { opacity: 1, scaleX: 2, backgroundColor: "#FFFFFF", duration: 1 }
+        );
+    },
+    change() {
+      let tl = this.$gsap.timeline();
+      tl.fromTo(
+        ".title-wrapper",
+        { opacity: 1, x: 0 },
+        { opacity: 0, duration: 1, x: -50 }
+      ).fromTo(
+        ".title-wrapper",
+        { x: -50, opacity: 0 },
+        { opacity: 1, duration: 1, x: 0 }
+      );
+    }
+  }
+};
+</script>
+
+<style  lang="scss" scoped >
+@import '../assets/styles/index.scss';
+</style>
